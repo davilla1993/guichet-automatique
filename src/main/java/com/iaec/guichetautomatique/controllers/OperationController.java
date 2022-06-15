0d82be69709429e0f645ab5caa30f52b9cb73008
davilla1993 <carlogbossou93@gmail.com>
2022-06-15 15:35:07 +0000

    15/06/2022

--- src/main/java/com/iaec/guichetautomatique/controllers/OperationController.java
+++ src/main/java/com/iaec/guichetautomatique/controllers/OperationController.java
@@ -1,15 +1,43 @@
 package com.iaec.guichetautomatique.controllers;
 
+import com.iaec.guichetautomatique.entities.Compte;
+import com.iaec.guichetautomatique.entities.Operation;
+import com.iaec.guichetautomatique.services.OperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OperationController {
-    @GetMapping("/operations")
+
+    private OperationService operationService;
+    @Autowired
+    public OperationController(OperationService operationService) {
+        this.operationService = operationService;
+    }
+    @RequestMapping("/operations")
     public String index() {
         return "admin/home";
     }
+    @RequestMapping("/consulterCompte")
+    public String consulter(Model model, int numCompte){
+        model.addAttribute("numCompte", numCompte);
 
+        try{
+            Compte compte = operationService.consulterCompte(numCompte);
+            Page<Operation> listOperations = operationService.listOperation(numCompte, 0, 6);
+
+            model.addAttribute("compte", compte);
+            model.addAttribute("listOperations", listOperations.getContent());
+
+        } catch(Exception ex){
+            String message = "Aucun compte n'a été trouvé avec ce numéro";
+            model.addAttribute("message", message);
+        }
+        return "admin/home";
+    }
 
 }
