7eea24027692e4c80e2952170e2453786d98e8d3
davilla1993 <carlogbossou93@gmail.com>
2022-06-26 22:57:43 +0000

    26/06/2022-heroku-deploy

--- src/main/java/com/iaec/guichetautomatique/GuichetAutomatiqueApplication.java
+++ src/main/java/com/iaec/guichetautomatique/GuichetAutomatiqueApplication.java
@@ -3,12 +3,11 @@ package com.iaec.guichetautomatique;
 import com.iaec.guichetautomatique.entities.Compte;
 import com.iaec.guichetautomatique.entities.Role;
 import com.iaec.guichetautomatique.entities.User;
+import com.iaec.guichetautomatique.repository.RoleRepository;
 import com.iaec.guichetautomatique.repository.UserRepository;
-import com.iaec.guichetautomatique.repository.CompteRepository;
 import com.iaec.guichetautomatique.services.RoleService;
 import com.iaec.guichetautomatique.services.UserService;
 import com.iaec.guichetautomatique.services.CompteService;
-import com.iaec.guichetautomatique.services.OperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -27,6 +26,8 @@ public class GuichetAutomatiqueApplication implements CommandLineRunner {
 	private UserService userService;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private RoleRepository roleRepository;
 
 	public static void main(String[] args) {
 
@@ -35,9 +36,8 @@ public class GuichetAutomatiqueApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		/*
 		// Création des roles
-		Role role_admin = new Role("ADMIN");
+		/*Role role_admin = new Role("ADMIN");
 		Role role_user =  new Role("USER");
 		roleService.createRole(role_admin);
 		roleService.createRole(role_user);
@@ -47,10 +47,14 @@ public class GuichetAutomatiqueApplication implements CommandLineRunner {
 		User user2 = new User("Utilisateur", "Test", "70986547", "user", "user");
 		User user3 = new User("GBOSSOU", "Folly", "91554874", "carlo", "1234");
 		User user4 = new User("ALADE", "Ghislaine", "95126790","ghis", "1234");
+		User user5 = new User("PEREKE", "Prince","93460373", "prince","1234");
+		User user6 = new User("DAOUDA","Ayélé Martine", "98670924","martine", "1234");
 		userService.create(user1);
 		userService.create(user2);
 		userService.create(user3);
 		userService.create(user4);
+		userService.create(user5);
+		userService.create(user6);
 
 		// Création des comptes bancaires
 		compteService.create(new Compte(15000, user2));
@@ -58,12 +62,12 @@ public class GuichetAutomatiqueApplication implements CommandLineRunner {
 		compteService.create(new Compte(45000, user3));
 		compteService.create(new Compte(90000, user4));
 
-
 		final List<Compte> comptesClient = compteService.getComptesByUser(user3.getId());
 			for(Compte compte : comptesClient){
-				System.out.println("Titulaire : " + compte.getUsers().getNom());
+				System.out.println("Titulaire : " + compte.getUser().getNom());
 				System.out.println("Numéro de compte:" + compte.getNumeroCompte());
 				System.out.println("Solde : " + compte.getSolde());
 			}*/
+
 	}
 }
