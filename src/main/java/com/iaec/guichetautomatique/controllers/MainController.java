520b1bdf1705450a8fcf1f7d4241bce351078b0e
davilla1993 <carlogbossou93@gmail.com>
2022-06-27 00:29:50 +0000

    27/06/2022-heroku-deploy

--- src/main/java/com/iaec/guichetautomatique/controllers/MainController.java
+++ src/main/java/com/iaec/guichetautomatique/controllers/MainController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @GetMapping("")
+    public String homePage(){
+        return "login";
+    }
     @GetMapping("/login")
     public String login(){
-
         return "login";
     }
 }
